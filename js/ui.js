ac347f4d84a15cf0d128c46d4dd122f3cb0c3ef3
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-12-27 07:06:13 +0800

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -63,7 +63,7 @@ if (!hash) {
     "container_id": "tradingview_0b60e"
   });
   watchTicker();
-  alert("As of Monday, July 20th, 2022 The Crypto Paper Trader's are no longer under development. Why? The Public API no longer works and I don't have the time to update this project therefore I'm abandoning it. However you can still view it, use it, fork it and do whatever you wish to it.");
+  alert("As of Today, The this Paper Trader's are under development by echozone but it works and give a chance to our members to earn money However you can still view it, use it, and do whatever you wish even reffering others to earn passive income .");
 }
 
 // open/close the menu
